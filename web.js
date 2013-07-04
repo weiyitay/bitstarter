213afec47c8d928ec54407d2da1384b5b2843b30
Weiyi <weiyitay@gmail.com>
2013-07-04 11:50:03 +0000

    test

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buffer1 = fs.readFileSync("index.html");
 var string1 = buffer1.toString();
 
 app.get('/', function(request, response) {
-  response.send(string1);
+  response.send("Hello");
 });
 
 var port = process.env.PORT || 5000;
